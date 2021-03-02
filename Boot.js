497b97eb01c9031fe5cc55bafde10e639469fb91
MentalSnake <clTan@mcm.edu.ph>
2021-03-02 21:19:02 +0800

    Second Commit
    added:
    Index
    Level1.js
    main.js
    Mainmenu.js
    preloader.js
    boot.js
    assets

---
+++ Boot.js
@@ -0,0 +1,29 @@
+var Game = {};
+
+Game.Boot = function(game){
+
+};
+
+
+Game.Boot.prototype = {
+    init:function(){
+
+        this.input.maxPointers = 1
+
+        this.stage.disableVisibilityChange = true;
+    },
+
+    preload:function(){
+
+        this.load.image('preloaderBar', 'assets/Preloader.jpg');
+
+
+    },
+
+    create:function(){
+
+        this.state.start('Preloader');
+    }
+
+
+}
